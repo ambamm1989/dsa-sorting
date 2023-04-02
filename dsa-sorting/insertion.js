056efd09ae73f89876cd6eb6a9c132cccd1c9c43
Adam Brammer <abrammer89@gmail.com>
2023-04-02 17:47:52 -0400

    Add files via upload

---
+++ dsa-sorting/insertion.js
@@ -0,0 +1,14 @@
+function insertionSort() {
+    for (let i = 0; i < arr.length; i++) {
+        let currentValue = arr[i];
+
+        for (let j = i -1 && arr[j] > currentValue; j--;) {
+            arr[j + 1] = arr[j];
+        }
+
+        arr[j + 1] = currentValue;
+    }
+
+    return arr;
+}
+module.exports = insertionSort;
